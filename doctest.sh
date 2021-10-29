322193a6ba11c9c7f57a3bc5f9dcb426c53f4124
Martin R. Albrecht <martinralbrecht@googlemail.com>
2021-10-29 19:59:50 +0100

    fix scripts

--- doctest.sh
+++ doctest.sh
@@ -2,11 +2,16 @@
 ###############################################################################
 #                              Run Sage doctests
 ###############################################################################
-SAGE_ROOT=$(sage -c "import os; print(os.environ['SAGE_ROOT'])")
-export SAGE_ROOT="$SAGE_ROOT"
+if [ "x$SAGE_ROOT" == "x" ]
+then
+    SAGE_ROOT=$(sage -c "import os; print(os.environ['SAGE_ROOT'])")
+    export SAGE_ROOT="$SAGE_ROOT"
+    export PATH="$SAGE_ROOT/bin:$SAGE_ROOT/src/bin:$PATH"
+    export SAGE_LOCAL="$SAGE_ROOT/local"
+    export DOT_SAGE=$HOME/.sage/
+fi
 
-# shellcheck source=/dev/null
-source "$SAGE_ROOT/local/bin/sage-env"
+
 for file in "$@"; do
     PYTHONIOENCODING=UTF-8 PYTHONPATH=$(pwd) sage-runtests "$file"
 done
--- fix-doctest.sh
+++ fix-doctest.sh
@@ -5,9 +5,13 @@
 # Please don't just blindly call this to make failures go away,
 # but review all changes.
 ###############################################################################
-SAGE_ROOT=$(sage -c "import os; print(os.environ['SAGE_ROOT'])")
-export SAGE_ROOT="$SAGE_ROOT"
+if [ "x$SAGE_ROOT" == "x" ]
+then
+    SAGE_ROOT=$(sage -c "import os; print(os.environ['SAGE_ROOT'])")
+    export SAGE_ROOT="$SAGE_ROOT"
+    export PATH="$SAGE_ROOT/bin:$SAGE_ROOT/src/bin:$PATH"
+    export SAGE_LOCAL="$SAGE_ROOT/local"
+    export DOT_SAGE=$HOME/.sage/
+fi
 
-# shellcheck source=/dev/null
-source "$SAGE_ROOT/local/bin/sage-env"
 PYTHONIOENCODING=UTF-8 PYTHONPATH=$(pwd) sage-fixdoctests "$@"
