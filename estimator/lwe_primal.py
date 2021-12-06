257bfc1be947a65f6292e25bbf87a083b8c671cd
malb <martinralbrecht@googlemail.com>
2021-12-06 14:38:32 +0000

    Update jupyter-notebooks to output generated at dfcc4b1

--- estimator/lwe_primal.py
+++ estimator/lwe_primal.py
@@ -366,7 +366,7 @@ class PrimalHybrid:
         )
 
         # 4. Repeat whole experiment ~1/prob times
-        if probability and not probability.is_NaN():
+        if probability and not RR(probability).is_NaN():
             ret = ret.repeat(
                 prob_amplify(0.99, probability),
             )
