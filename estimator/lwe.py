84a90fdccf9469e755a119f519085bf90c92dd6f
Martin R. Albrecht <martinralbrecht@googlemail.com>
2022-01-05 10:22:53 +0000

    don't add arora-gb to the deny list by default

--- estimator/lwe.py
+++ estimator/lwe.py
@@ -82,7 +82,7 @@ class Estimate:
         params,
         red_cost_model=None,
         red_shape_model=None,
-        deny_list=("arora-gb",),
+        deny_list=tuple(),
         add_list=tuple(),
         jobs=1,
     ):
@@ -100,6 +100,7 @@ class Estimate:
 
             >>> from estimator import *
             >>> _ = lwe.estimate(Kyber512)
+            arora-gb             :: rop: ≈2^inf, dreg: 25, mem: ≈2^106.3, t: 3, m: ≈2^inf, tag: arora-gb, ↻: ≈2^inf, ...
             bkw                  :: rop: ≈2^178.8, m: ≈2^166.8, mem: ≈2^167.8, b: 14, t1: 0, t2: 16, ℓ: 13, #cod: 448...
             usvp                 :: rop: ≈2^148.0, red: ≈2^148.0, δ: 1.003941, β: 406, d: 998, tag: usvp
             bdd                  :: rop: ≈2^144.5, red: ≈2^143.8, svp: ≈2^143.0, β: 391, η: 421, d: 1013, tag: bdd
