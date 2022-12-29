8430aaba798196e87a905ead341b02ddd311a05a
malb <martinralbrecht@googlemail.com>
2022-12-29 16:45:15 +0000

    Update jupyter-notebooks to output generated at 6a96c61

--- estimator/lwe.py
+++ estimator/lwe.py
@@ -4,18 +4,23 @@ High-level LWE interface
 """
 
 from functools import partial
+from sage.all import oo
+
 from .lwe_primal import primal_usvp, primal_bdd, primal_hybrid
 from .lwe_bkw import coded_bkw
-from .lwe_guess import exhaustive_search, mitm, distinguish  # noqa
+from .lwe_guess import exhaustive_search, mitm, distinguish, guess_composition # noqa
 from .lwe_dual import dual, dual_hybrid
-from .lwe_guess import guess_composition
 from .gb import arora_gb  # noqa
 from .lwe_parameters import LWEParameters as Parameters  # noqa
+from .conf import (red_cost_model as red_cost_model_default,
+                   red_shape_model as red_shape_model_default)
+from .util import batch_estimate, f_name
+from .reduction import RC
 
 
 class Estimate:
-    @classmethod
-    def rough(cls, params, jobs=1, catch_exceptions=True):
+
+    def rough(self, params, jobs=1, catch_exceptions=True):
         """
         This function makes the following somewhat routine assumptions:
 
@@ -42,12 +47,6 @@ class Estimate:
 
 
         """
-        # NOTE: Don't import these at the top-level to avoid circular imports
-        from .reduction import RC
-        from .util import batch_estimate, f_name
-
-        from sage.all import oo
-
         params = params.normalize()
 
         algorithms = {}
@@ -83,18 +82,19 @@ class Estimate:
         }
 
         for algorithm in algorithms:
-            for k, v in res.items():
-                if algorithm == k:
-                    if v["rop"] == oo:
-                        continue
-                    print(f"{algorithm:20s} :: {v!r}")
+            if algorithm not in res:
+                continue
+            result = res[algorithm]
+            if result["rop"] != oo:
+                print(f"{algorithm:20s} :: {result!r}")
+
         return res
 
     def __call__(
         self,
         params,
-        red_cost_model=None,
-        red_shape_model=None,
+        red_cost_model=red_cost_model_default,
+        red_shape_model=red_shape_model_default,
         deny_list=tuple(),
         add_list=tuple(),
         jobs=1,
@@ -124,18 +124,7 @@ class Estimate:
             dual_hybrid          :: rop: ≈2^145.6, mem: ≈2^140.5, m: 512, β: 408, d: 1004, ↻: 1, ζ: 20, tag: dual_hybrid
 
         """
-        from sage.all import oo
-        from functools import partial
-        from .conf import red_cost_model as red_cost_model_default
-        from .conf import red_shape_model as red_shape_model_default
-        from .util import batch_estimate, f_name
-
         params = params.normalize()
-
-        if red_cost_model is None:
-            red_cost_model = red_cost_model_default
-        if red_shape_model is None:
-            red_shape_model = red_shape_model_default
 
         algorithms = {}
 
@@ -179,21 +168,24 @@ class Estimate:
         )
         res_raw = res_raw[params]
         res = {
-            algorithm: v for algorithm, attack in algorithms.items()
+            algorithm: v
+            for algorithm, attack in algorithms.items()
             for k, v in res_raw.items()
             if f_name(attack) == k
         }
 
         for algorithm in algorithms:
-            for k, v in res.items():
-                if algorithm == k:
-                    if v["rop"] == oo:
-                        continue
-                    if k == "hybrid" and res["bdd"]["rop"] < v["rop"]:
-                        continue
-                    if k == "dual_mitm_hybrid" and res["dual_hybrid"]["rop"] < v["rop"]:
-                        continue
-                    print(f"{algorithm:20s} :: {v!r}")
+            if algorithm not in res:
+                continue
+            result = res[algorithm]
+            if result["rop"] == oo:
+                continue
+            if algorithm == "hybrid" and res["bdd"]["rop"] < result["rop"]:
+                continue
+            if algorithm == "dual_mitm_hybrid" and res["dual_hybrid"]["rop"] < result["rop"]:
+                continue
+            print(f"{algorithm:20s} :: {result!r}")
+
         return res
 
 
