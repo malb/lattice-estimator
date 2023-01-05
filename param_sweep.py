0c964f41ba5c29e20486ebd46b0d9c55437345ee
Cathie Yun <3314874+cathieyun@users.noreply.github.com>
2023-01-04 18:02:45 -0800

    Add cutoff graph

--- param_sweep.py
+++ param_sweep.py
@@ -8,27 +8,28 @@ Required arguments:
   - list: looks like -n=25,70,80,100
   - range: looks like -n=25:125:25
   - single value: looks like -n=200
-* -e or --sigma_e: Value(s) for sigma of error e, as inputs to a discrete
+* -e or --sigma_e: Value(s) for log_2(sigma) of error e, as inputs to a discrete
   gaussian distribution (unless specified otherwise.) This can be a
   - list: looks like -e=6.1,7.2,7.5 or --sigma_e=6,7,8
   - range: looks like -e=6:8:0.2 or --sigma-e=6:8 (increments by 1)
   - single value: looks like --sigma_e=200
-* -s or --sigma_s: Value(s) for sigma of secret s, as inputs to a discrete
-  gaussian distribution (unless specified otherwise.) This can be a
+* -s or --sigma_s: Value(s) for log_2(sigma) of secret s, as inputs to a
+  discrete gaussian distribution (unless specified otherwise.) This can be a
   - list: looks like -s=6.1,7.2,7.5 or --sigma_s=6,7,8
   - range: looks like -s=6:8:0.2 or --sigma-s=6:8 (increments by 1)
   - single value: looks like --sigma_s=200 
 
 Example command for a simple sweep:
 * n=600 to 800, by increments of 20
-* Xe from 6 to 20, by increments of 1 (discrete gaussian)
-* Xs as 5 (discrete gaussian)
+* log_2(Xe) from 6 to 20, by increments of 1 (discrete gaussian)
+* log_2(Xs) as 5 (discrete gaussian)
   sage --python param_sweep.py -n=600:800:20 -e=6:20 -s=5 -f
 
 Optional flags:
 * -u or --uniform_s: Whether to use a uniform distribution for secret key s.
-  If unset, defaults to using a discrete gaussian distribution. Useful for FHE
-  schemes.
+  If so, make a uniform distribution over the value passed in by `-s`.
+  If unset, defaults to a discrete gaussian distribution over log_2(sigma).
+  Useful for FHE schemes.
 * -q: Value(s) for the modulus q. This can be a
   * list: looks like -q=25,70,80,100
   * range: looks like -q=25:125:25
@@ -54,7 +55,7 @@ Example command using many of the flags, generating a pickle file:
 
 Example command to generate graphs from a pre-existing pickle file:
   sage --python param_sweep.py -n=600:1140:20 -e=6:20 -s=2 --uniform_s \
-  --load_pickle=path/to/file
+  --load_pickle='param_sweep/{filename}.pickle'
 
 Note: running the parameter sweep can take minutes to hours, depending on the
 number of parameter combinations (the larger the ranges and the smaller the 
@@ -62,19 +63,19 @@ increment intervals, the more parameter combinations will be computed). For
 faster and rougher results, use the --fast flag.
 """
 
-from typing import Iterable, Union
-import argparse
 from estimator import LWE
 from estimator.io import Logging
 from estimator.lwe_parameters import LWEParameters
 from estimator.nd import NoiseDistribution as ND
+from matplotlib import pyplot as plt
+from typing import Iterable, Union
+import argparse
+import pickle
 import time
 import math
 import multiprocessing
 import numpy as np
 import os
-import pickle
-from matplotlib import pyplot as plt
 
 
 def parse_arg(arg_val: str) -> Iterable:
@@ -97,8 +98,9 @@ def parse_arg(arg_val: str) -> Iterable:
 
 
 def GraphResults(result_dict: dict,
-                params: dict[str, list[Union[int, float]]],
+                params: dict[str, (list[Union[int, float]], int)],
                 file_name: str,
+                security_cutoff: int,
                 log_level: int):
     """
     Creates one or more graphs, depending on how many variables are being
@@ -118,12 +120,11 @@ def GraphResults(result_dict: dict,
     Returns:
         None
     """
-    params_to_idx = {'n': 0, 'q': 1, 'Xe': 2, 'Xs': 3}
+    
     # First, figure out how many variables we need to plot.
     axis_vars = {}
-
     for param in params:
-        if len(params[param]) > 1:
+        if len(params[param][0]) > 1:
             axis_vars[param] = len(params[param])
 
     if len(axis_vars) == 0:
@@ -136,15 +137,15 @@ def GraphResults(result_dict: dict,
 
     elif len(axis_vars) == 1:
         variable_param = list(axis_vars.keys())[0]  # a string like 'Xe'
-        x = sorted(params[variable_param])
-        y = sorted(result_dict.items(), key=lambda x: x[params_to_idx[variable_param]])
+        x = sorted(params[variable_param][0])
+        y = sorted(result_dict.items(), key=lambda x: x[params[variable_param][1]])
         y = [_ for _ in zip(*y)][1]
         fig, ax = plt.subplots(figsize=(20, 20), dpi=80)
 
         ax.plot(x, y)
         ax.set_xlabel(f'Parameter: {variable_param}')
         ax.set_ylabel('Security')
-        desc_params = {i:v[0] for i, v in params.items() if len(v) == 1}
+        desc_params = {i:v[0] for i, (v, _) in params.items() if len(v) == 1}
         plt.title(f'Security with parameters {desc_params}')
 
         Logging.log('sweep', log_level, 'Saved the line plot graph to: %s',
@@ -152,11 +153,11 @@ def GraphResults(result_dict: dict,
         fig.savefig(file_name + '_plot.png')
 
     elif len(axis_vars) == 2:
-        axis_vars = sorted(axis_vars.items(), key=lambda x: params_to_idx[x[0]])
+        axis_vars = sorted(axis_vars.items(), key=lambda x: params[x[0]][1])
         x_param = axis_vars[1][0]
-        x = sorted(params[x_param])
+        x = sorted(params[x_param][0])
         y_param = axis_vars[0][0]
-        y = sorted(params[y_param])
+        y = sorted(params[y_param][0])
 
         values = [i[1] for i in sorted(result_dict.items(), key=lambda x: x[0])]
         mat = np.flip(np.array(values).reshape(
@@ -171,7 +172,7 @@ def GraphResults(result_dict: dict,
         ax.set_xticklabels(sorted(list(set(x))))
         ax.set_yticklabels(sorted(list(set(y)), reverse=True))
 
-        desc_params = {i:v[0] for i, v in params.items() if len(v) == 1}
+        desc_params = {i:v[0] for i, (v, _) in params.items() if len(v) == 1}
         plt.title(f'Security with parameters {desc_params}')
 
         for (j, i), label in np.ndenumerate(mat):
@@ -184,6 +185,35 @@ def GraphResults(result_dict: dict,
         Logging.log('sweep', log_level, 'Saved the gradient graph to: %s',
                     file_name + '_gradient.png')
 
+        if security_cutoff:
+            fig2, ax2 = plt.subplots(figsize=(20, 20), dpi=80)
+            binary_mat = mat >= security_cutoff
+            ax2.imshow(binary_mat)
+            ax2.set_xticks(np.arange(0, len(set(x)), 1))
+            ax2.set_yticks(np.arange(0, len(set(y)), 1))
+            ax2.set_xticklabels(sorted(list(set(x))))
+            ax2.set_yticklabels(sorted(list(set(y)), reverse=True))
+
+            for (j, i), label in np.ndenumerate(mat):
+                ax2.text(i,
+                         j,
+                         round(label, 1),
+                         ha='center',
+                         va='center',
+                         color='white' if label < security_cutoff else 'black')
+
+            for (j, i), label in np.ndenumerate(mat):
+                ax.text(i, j, round(label, 1), ha='center', va='center', color='white')
+            plt.title(f'Security with parameters {desc_params}'
+                      f' and cutoff at {security_cutoff}')
+
+            ax2.set_xlabel(f'Parameter: {x_param}')
+            ax2.set_ylabel(f'Parameter: {y_param}')
+
+            fig2.savefig(file_name + '_cutoff.png')
+            Logging.log('sweep', log_level, 'Saved the cutoff graph to: %s',
+                        file_name + '_cutoff.png')
+
 
 def PerformCalculation(input_params: tuple[int, float],
                        results: dict,
@@ -195,15 +225,17 @@ def PerformCalculation(input_params: tuple[int, float],
     parameters, and appends the output to the `results` dict. 
     """
     Logging.log('sweep', log_level, f'Running for n, q, Xe, Xs = {input_params}')
-    Xs_ = ND.DiscreteGaussian if uniform_s else ND.UniformMod
+    Xs_ = ND.UniformMod(input_params[2]) if uniform_s \
+        else ND.DiscreteGaussian(2**input_params[2])
     f_ = LWE.estimate.rough if fast else LWE.estimate
     params = LWE.Parameters(
         n=int(input_params[0]),
         q=int(input_params[1]),
-        Xe=ND.DiscreteGaussian(input_params[2]),
-        Xs=Xs_(input_params[3]),
+        Xe=ND.DiscreteGaussian(2**input_params[2]),
+        Xs=Xs_,
         m=input_params[0],
     )
+    print("params: ", params)
     results[input_params] = security_level(params, f=f_)
 
 
@@ -225,8 +257,8 @@ if __name__ == "__main__":
     parser.add_argument('-e', '--sigma_e',
                         type=str,
                         help=
-        'Value(s) for sigma of error e, as inputs to a discrete gaussian '
-        'distribution (unless specified otherwise.) This can be a\n'
+        'Value(s) for log_2(sigma) of error e, as inputs to a discrete '
+        'gaussian distribution (unless specified otherwise.) This can be a\n'
         '* list: looks like -e=6.1,7.2,7.5 or --sigma_e=6,7,8\n'
         '* range: looks like -e=6:8:0.2 or --sigma-e=6:8 (increments by 1)\n'
         '* single value: looks like --sigma_e=5\n',
@@ -234,8 +266,8 @@ if __name__ == "__main__":
     parser.add_argument('-s', '--sigma_s',
                         type=str,
                         help=
-        'Value(s) for sigma of secret s, as inputs to a discrete gaussian '
-        'distribution (unless specified otherwise.) This can be a\n'
+        'Value(s) for log_2(sigma) of secret s, as inputs to a discrete '
+        'gaussian distribution (unless specified otherwise.) This can be a\n'
         '* list: looks like -s=6.1,7.2,7.5 or --sigma_s=6,7,8\n'
         '* range: looks like -s=6:8:0.2 or --sigma-s=6:8 (increments by 1)\n'
         '* single value: looks like --sigma_s=5\n',
@@ -258,6 +290,13 @@ if __name__ == "__main__":
         'Use LWE.Estimate.Rough, for a faster estimate. '
         'If unset, defaults to using LWE.Estimate which is slower.',
                         action='store_true')
+    parser.add_argument(
+        '--security_cutoff',
+        type=int,
+        help='Generate a second graph, with parameters that give more security '
+        'than the security cutoff labeled with a different value in the graph',
+        default=0,
+        required=False)
     parser.add_argument(
         '--output_dir',
         type=str,
@@ -319,8 +358,17 @@ if __name__ == "__main__":
     else:
         result_dict = pickle.load(open(args.load_pickle, 'rb'))
 
+    s_string = 's (uniform dist) mod' if args.uniform_s else 'log_2(Xs)'
+    params_dict = {
+        'n': (n, 0),
+        'q': (q, 1),
+        'log_2(Xe)': (sigma_e, 2),
+        s_string: (sigma_s, 3)
+    }
+
     GraphResults(
         result_dict,
-        {'n': n, 'q': q, 'Xe': sigma_e, 'Xs': sigma_s},
+        params_dict,
         file_name,
+        int(args.security_cutoff),
         log_level)
