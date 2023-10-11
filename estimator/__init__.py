f3a41280967593175116c2389d73478f09b42ed7
Hunter Kippen <hunter.kippen@sandboxquantum.com>
2023-10-10 20:19:57 -0400

    changed order of __init__.py to fix sphinx docgen

--- estimator/__init__.py
+++ estimator/__init__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
-__all__ = ['ND', 'Logging', 'RC', 'Simulator', 'LWE', 'schemes', 'NTRU']
+__all__ = ['ND', 'Logging', 'RC', 'Simulator', 'LWE', 'NTRU', 'schemes']
 
 from .nd import NoiseDistribution as ND
 from .io import Logging
 from .reduction import RC
 from . import simulator as Simulator
 from . import lwe as LWE
-from . import schemes
 from . import ntru as NTRU
+from . import schemes
